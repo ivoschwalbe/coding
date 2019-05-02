311ff73283a2978b46c4436919fbbb7b1aa4cea4
Ivo Schwalbe <ivo@ivo-s.de>
2019-05-02 10:13:37 +0200

    Hahaha

--- ex40.py
+++ ex40.py
@@ -1,24 +1,30 @@
 class Song(object):
-    def __init__(self, lyrics):
+    def __init__(self, lyrics, name):
         self.lyrics = lyrics
+        self.name = name
 
     def sing_me_a_song(self):
         for line in self.lyrics:
             print(f"{line}")
+
+    def count_lines(self):
+        print(f"{self.name} hat {len(self.lyrics)} Zeilen!")
 
 happy_bday = Song(
     [
         "Happy birthday to you",
         "I don't want to get sued",
         "So I'll stop right there",
-    ]
+    ],
+    "Happy Birthday"
 )
 
 bulls_on_parade = Song(
     [
         "They rally around tha family",
         "With pockets full of shells",
-    ]
+    ],
+    "Bulls on parade"
 )
 
 ein_anderer_song = [
@@ -29,10 +35,14 @@ ein_anderer_song = [
     "Mit dem Schießgewehr ..."
 ]
 
-fuch_dhdGg = Song(ein_anderer_song)
+fuch_dhdGg = Song(ein_anderer_song, "Fuchs du hast die Gans gestohlen")
 
 happy_bday.sing_me_a_song()
 print("# " * 10)
 bulls_on_parade.sing_me_a_song()
 print("# " * 10)
 fuch_dhdGg.sing_me_a_song()
+print("# " * 10)
+happy_bday.count_lines()
+fuch_dhdGg.count_lines()
+bulls_on_parade.count_lines()
